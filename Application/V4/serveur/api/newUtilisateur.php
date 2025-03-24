074974e7754f0a218279f79b2df8b4dc1e5ad1ee
Loric <loric.bondon1@etu.univ-lorraine.fr>
2025-03-24 19:09:41 +0100

    api connexion + newUtilisateur

---
+++ Application/V4/serveur/api/newUtilisateur.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+require_once "../bdd/connexion.php";
+require_once 'header.php';
+
+$json = [];
+
+$query =
+"INSERT INTO USER (id_user, id_perm, nom, prenom, email, date_naissance, identifiant, mdp)
+VALUES (NULL, 2, :nom, :prenom, :email, :date_naissance, :identifiant, :mdp)";
+
+$res = $db->prepare($query);
+$res->bindParam(':nom', $_POST['nom']);
+$res->bindParam(':prenom', $_POST['prenom']);
+$res->bindParam(':email', $_POST['email']);
+$res->bindParam(':date_naissance', $_POST['date_naissance']);
+$res->bindParam(':identifiant', $_POST['identifiant']);
+$res->bindParam(':mdp', password_hash($_POST['mdp'], PASSWORD_DEFAULT));
+
+try {
+    $res->execute();
+    $json["status"] = "success";
+    $json["message"] = "Insertion réussie";
+
+} catch(Exception $exception) {
+    $json["status"] = "error";
+    $json["message"] = $exception->getMessage();
+}
+
+echo json_encode($json);
