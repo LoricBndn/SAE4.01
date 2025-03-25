2de256dd94443cdb3462d7896e309449b7e0f5fe
Loric <loric.bondon1@etu.univ-lorraine.fr>
2025-03-25 12:13:52 +0100

    api getCategorie (d'un produit) et getCategories

--- Application/V4/serveur/api/getCategorie.php
+++ Application/V4/serveur/api/getCategorie.php
@@ -5,25 +5,24 @@ require_once 'header.php';
 
 $json = [];
 
-$query = "SELECT id_prod ,nom_prod, prix_base
-FROM CATEGORIE,PRODUIT 
-WHERE CATEGORIE.id_cat = PRODUIT.id_cat 
-AND PRODUIT.id_cat = :id_cat"; //on peut mettre le nom de la catégorie en paramètre mais pas sur que ca soit utile
+$query = 
+"SELECT C.id_categorie, nom_categorie
+FROM CATEGORIE C, PRODUIT P
+WHERE C.id_categorie = P.id_categorie
+AND P.id_produit = :id_produit";
 
 $res = $db->prepare($query);
-$res->bindParam(':id_cat', $_POST['id_cat']);
+$res->bindParam(':id_produit', $_POST['id_produit']);
 
 try {
     $res->execute();
     $json["status"] = "success";
-    $json["message"] = "Insertion réussie";
+    $json["message"] = "Sélection réussie";
     $json["data"] = $res->fetchAll(PDO::FETCH_ASSOC);
-
 } catch(Exception $exception) {
     $json["status"] = "error";
     $json["message"] = $exception->getMessage();
     $json["data"] = "[]";
 }
-
 
 echo json_encode($json);
