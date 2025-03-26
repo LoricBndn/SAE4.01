3abf1daa3729e056ca99f0bb632e7f3e3966a141
Loric <loric.bondon1@etu.univ-lorraine.fr>
2025-03-26 15:58:08 +0100

    Update getGenericProduit.php

--- Application/V4/serveur/api/getGenericProduit.php
+++ Application/V4/serveur/api/getGenericProduit.php
@@ -7,10 +7,12 @@ $query =
 "SELECT *
 FROM SELECT_PRODUITS
 WHERE id_produit = :id_produit
+AND id_couleur = :id_couleur
 GROUP BY id_produit, id_couleur";
 
 $res = $db->prepare($query);
 $res->bindParam(":id_produit", $_POST["id_produit"]);
+$res->bindParam(":id_couleur", $_POST["id_couleur"]);
 
 try {
     $res->execute();
