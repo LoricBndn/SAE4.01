919213b4b46d46f3b99bfc6eed7e1a97ca42c5ac
Loric <loric.bondon1@etu.univ-lorraine.fr>
2025-03-27 16:34:14 +0100

    correction lien page rgpd

--- Application/V4/client/controleur/function.js
+++ Application/V4/client/controleur/function.js
@@ -112,7 +112,7 @@ function printFooter() {
 
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
                 <li>
-                    <a href="./mentionsLegales.html class="hover:underline me-4 md:me-6">Mentions Légales</a>
+                    <a href="./mentionsLegales.html" class="hover:underline me-4 md:me-6">Mentions Légales</a>
                 </li>
             </ul>
         </div>
