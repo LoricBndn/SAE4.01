b60b910a5cb5e5976ff7b80109773dd04560474f
SevanDev <93271849+Sevan-Dev@users.noreply.github.com>
2025-03-27 19:13:06 +0100

    Commit

--- Application/V4/client/controleur/panier.js
+++ Application/V4/client/controleur/panier.js
@@ -285,29 +285,47 @@ async function displayProduits(produits) {
 
     };
 
-    const prixContainer = document.createElement("div");
-    prixContainer.classList.add('bg-gray-100', 'rounded-xl', 'p-6', 'w-80', 'mb-8', 'max-lg:max-w-xl', 'max-lg:mx-auto', 'ml-auto');
-    prixContainer.innerHTML = `
-        <div class="flex items-center justify-between w-full py-2">
-            <p class="font-medium text-2xl leading-9 text-gray-900">
-              Total 
-            </p>
-            <h6 class="font-medium text-2xl leading-9">
-              ${total}€
-            </h6>
-        </div>
-        <div class="flex items-center flex-col sm:flex-row justify-center gap-3 mt-8">
- <button id="paymentButton" class="cursor-pointer w-full max-w-[280px] py-4 text-center justify-center items-center bg-[#B43131] font-semibold text-lg text-white flex transition-all duration-500 hover:bg-[#9b2929] rounded-md">
-            Passer au paiement
-            <svg class="ml-2" xmlns="http://www.w3.org/2000/svg" width="23" height="22" viewBox="0 0 23 22" fill="none">
-                <path d="M8.75324 5.49609L14.2535 10.9963L8.75 16.4998" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-        </button>
-        </div>
-    `;
-
-    produitsContainer.appendChild(prixContainer);
-    produitsContainer.appendChild(prixContainer);
+    if (total > 0) {
+        const prixContainer = document.createElement("div");
+        prixContainer.classList.add('bg-gray-100', 'rounded-xl', 'p-6', 'w-80', 'mb-8', 'max-lg:max-w-xl', 'max-lg:mx-auto', 'ml-auto');
+        prixContainer.innerHTML = `
+            <div class="flex items-center justify-between w-full py-2">
+                <p class="font-medium text-2xl leading-9 text-gray-900">
+                  Total 
+                </p>
+                <h6 class="font-medium text-2xl leading-9">
+                  ${total}€
+                </h6>
+            </div>
+            <div class="flex items-center flex-col sm:flex-row justify-center gap-3 mt-8">
+                <button id="paymentButton" class="cursor-pointer w-full max-w-[280px] py-4 text-center justify-center items-center bg-[#B43131] font-semibold text-lg text-white flex transition-all duration-500 hover:bg-[#9b2929] rounded-md">
+                    Passer au paiement
+                    <svg class="ml-2" xmlns="http://www.w3.org/2000/svg" width="23" height="22" viewBox="0 0 23 22" fill="none">
+                        <path d="M8.75324 5.49609L14.2535 10.9963L8.75 16.4998" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </button>
+            </div>
+        `;
+        produitsContainer.appendChild(prixContainer);
+    } else {
+        const prixContainer = document.createElement("div");
+        prixContainer.classList.add('bg-gray-100', 'rounded-xl', 'p-6', 'w-80', 'mb-8', 'max-lg:max-w-xl', 'max-lg:mx-auto', 'ml-auto');
+        prixContainer.innerHTML = `
+            <div class="flex items-center justify-between w-full py-2">
+                <p class="font-medium text-2xl leading-9 text-gray-900">
+                  Total 
+                </p>
+                <h6 class="font-medium text-2xl leading-9">
+                  0€
+                </h6>
+            </div>
+            <div class="flex items-center justify-center">
+                <p class="text-lg text-gray-500">Votre panier est vide.</p>
+            </div>
+        `;
+        produitsContainer.appendChild(prixContainer);
+    }
+    
 
     const paymentButton = document.getElementById("paymentButton");
     if (paymentButton) {
